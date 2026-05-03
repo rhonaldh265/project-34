8b25f3cf5cb8297a7490e1beb1956feaca34668b
rhonaldh265 <191988247+rhonaldh265@users.noreply.github.com>
2026-05-03 15:01:23 +0300

    Set up MongoDB connection in db.php
    Establish MongoDB connection and handle exceptions.

--- db.php
+++ db.php
@@ -1,1 +1,14 @@
+<?php
+require 'vendor/autoload.php';
 
+// Replace with your real Atlas connection string
+$connectionString = "mongodb+srv://<username>:<password>@cluster.mongodb.net/";
+
+try {
+    $client = new MongoDB\Client($connectionString);
+    // This will create a database named 'auth_db' and a collection 'users'
+    $collection = $client->auth_db->users;
+} catch (Exception $e) {
+    die("Error connecting to database: " . $e->getMessage());
+}
+?>
